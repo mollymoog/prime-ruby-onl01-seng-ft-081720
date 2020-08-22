25f696fea35ba5ceac88e3db1e7244739bfbf68d
molly mk <mmkover@gmail.com>
2020-08-22 18:10:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  i = 2 
+  
+  if number > 1 
+    range = (i..number-1).to_a
+    range.none? do |integer|
+      number % integer == 0 
+    end
+  else 
+    false 
+end
+end
