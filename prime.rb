4c4a56ce259a3533907edf06d8e79159a3ea5c34
molly mk <mmkover@gmail.com>
2020-08-22 18:09:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   i = 2 
   
   if number > 1 
-    range = (start..number-1).to_a
+    range = (i..number-1).to_a
     range.none? do |integer|
       number % integer == 0 
     end
