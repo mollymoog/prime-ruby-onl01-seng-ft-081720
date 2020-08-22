a4683ac139e2e9f8ef7dd9a8911ecf019568c19b
molly mk <mmkover@gmail.com>
2020-08-22 18:05:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,4 @@
 def prime?(number)
-  i = 1
-  ii = Integer.sqrt(number)
-  if number == 0 
-    false
-  elsif number == 1 
-    false
-  elsif number < 3 
-    true 
-  else  
-    true
-  end
+  i = 2 
+  
 end
